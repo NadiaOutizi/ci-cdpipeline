c459afbabaebfcfd6b9dae2ae9c7f11dae27db9c
Nadia Outizi <107075774+NadiaOutizi@users.noreply.github.com>
2024-06-05 11:23:19 +0100

    nadia push

--- src/main/java/com/helloNadia/helloNadia/controllers/Hello.java
+++ src/main/java/com/helloNadia/helloNadia/controllers/Hello.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
-@RequestMapping("nadia")
+@RequestMapping("/api")
 public class Hello {
 
 
-    @GetMapping
+    @GetMapping("/nadiaoutizi")
     public String working(){
         return "nadia from marrakech";
     }
